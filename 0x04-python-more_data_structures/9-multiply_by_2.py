3f50b2c38c38176950cd66a99afaedc115b5e075
O.B.O <125463724+OussamaBaOu@users.noreply.github.com>
2023-06-15 04:26:34 +0200

    python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     a = a_dictionary.copy()
-    b = list(new_dir.keys())
+    b = list(a.keys())
     for c in b:
         a[c] *= 2
     return (a)
