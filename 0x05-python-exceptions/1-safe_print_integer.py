fc51bd34eecbc17daf1db9dac03bfe58df6ccf15
OussamaBaOu <blondblynd@gmail.com>
2023-06-26 10:44:51 -0700

    py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    a = 0
+    for b in range(0, x):
+        try:
+            print("{:d}".format(my_list[b]), end="")
+            a += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (a)
