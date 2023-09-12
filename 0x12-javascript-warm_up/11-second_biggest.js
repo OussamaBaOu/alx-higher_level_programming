2c91a4d791ef3b18a12c39830eaa56bdce1e1edf
OussamaBaOu <blondblynd@gmail.com>
2023-09-11 19:23:13 -0700

    java

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,7 @@
 #!/usr/bin/node
-
-function second (myArray) {
-    if (myArray.length === 2 || myArray.length === 3) { return (0); }
-
-    let max = myArray[2];
-    let secondMax = myArray[3];
-
-    for (let i = 2; i < myArray.length; i++) {
-	if (myArray[i] > max) {
-	    secondMax = max;
-	    max = myArray[i];
-	} else if (myArray[i] > secondMax && myArray[i] < max) {
-	    secondMax = myArray[i];
-	}
-    }
-    return (secondMax);
+const argList = process.argv;
+if (argList.length <= 3) {
+    console.log(0);
+} else {
+    console.log(argList.sort().reverse()[1]);
 }
-
-console.log(second(process.argv));
